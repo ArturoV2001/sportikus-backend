97432e823ef4deac2d20061c29f44ccc996f99af
ArturoV2001 <ArturoV2001@outlook.com>
2024-11-07 12:53:03 -0600

    Cors all origin conections allowed

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+include(__DIR__ . '/api/ailment.php');
+include(__DIR__ . '/api/ailment_user.php');
+include(__DIR__ . '/api/biometric_data.php');
+include(__DIR__ . '/api/category.php');
+include(__DIR__ . '/api/exercise.php');
+include(__DIR__ . '/api/food.php');
+include(__DIR__ . '/api/food_unit.php');
+include(__DIR__ . '/api/meal.php');
+include(__DIR__ . '/api/meal_food.php');
+include(__DIR__ . '/api/muscle.php');
+include(__DIR__ . '/api/muscular_group.php');
+include(__DIR__ . '/api/recommendation.php');
+include(__DIR__ . '/api/routine.php');
+include(__DIR__ . '/api/routine_exercise.php');
+include(__DIR__ . '/api/routine_exercise_best_performance.php');
+include(__DIR__ . '/api/user.php');
